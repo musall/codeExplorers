4a2514cbc24ba68f5eb469b5254035415372fe32
Simon Musall <49614434+musall@users.noreply.github.com>
2024-07-31 19:03:58 +0200

    Update cE_demoScript_AJ.py

--- Playground/Anoushka/cE_demoScript_AJ.py
+++ Playground/Anoushka/cE_demoScript_AJ.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate data for a sine wave
-x = np.linspace(0, 50 * np.pi, 100)
+x = np.linspace(0, 60 * np.pi, 100)
 y = np.sin(x)
 
 # Create the plot
